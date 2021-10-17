5c7f9eda0d47c53664b01fcddf29432dad76e389
kim-doan <doan960105@gmail.com>
2021-10-17 16:25:15 +0900

    no message

--- portfolio/lib/app_config.dart
+++ portfolio/lib/app_config.dart
@@ -1,3 +1,4 @@
 class AppConfig {
-  final String baseURL = "http://192.168.10.68:9111/api"; // 로컬
+  // final String baseURL = "http://192.168.10.68:9111/api"; // 로컬
+  final String baseURL = "http://127.0.0.1:9111/api";
 }
