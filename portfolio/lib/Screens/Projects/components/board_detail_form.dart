338b3c58f1729e15e873f613d239ef372ee45531
kim-doan <doan960105@gmail.com>
2022-03-12 18:16:16 +0900

    프로젝트 상세보기에서 URL 동작 안하는 문제 해결

--- portfolio/lib/Screens/Projects/components/board_detail_form.dart
+++ portfolio/lib/Screens/Projects/components/board_detail_form.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_html/flutter_html.dart';
 import 'package:intl/intl.dart';
 import 'package:portfolio/Model/board_model.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:html/dom.dart' as dom;
 
 class BoardDetailForm extends StatelessWidget {
   const BoardDetailForm({Key? key, required this.board}) : super(key: key);
@@ -11,7 +13,17 @@ class BoardDetailForm extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
 
-    Widget html = Html(data: board.boardDetail.contents);
+    Widget html = Html(
+        data: board.boardDetail.contents,
+        onLinkTap: (String? url, RenderContext context, Map<String, String> attributes, dom.Element? element) async {
+          if (await canLaunch(url!)) {
+            await launch(
+              url,
+            );
+          } else {
+            throw 'Could not launch $url';
+          }
+        });
 
     return Container(
       width: size.width > 715 ? size.width / 2 : size.width,
